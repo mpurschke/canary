ad7f9bbddd8bf641a94a7cc43e8d3cefba2a9bc9
Martin L Purschke <purschke@bnl.gov>
2023-09-01 01:33:45 -0400

    mlp - added the canary special features

--- xdrs.cc
+++ xdrs.cc
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <pmonitor/pmonitor.h>
+#include <Event/oncsSubConstants.h>
 #include "xdrs.h"
 
 #include <TH1.h>
@@ -16,10 +17,14 @@ using namespace std;
 
 TH1F *trace[4];
 
+TH1F *h_signal;
+TH1F *tdelta;
+
 TH2F *x2;
 
+unsigned long long  res = 1000000000;   // from clockres system call
 
-int packetid=1001;
+int packetid=4500;
 
 int pinit()
 {
@@ -46,8 +51,18 @@ int pinit()
   trace[1]->SetLineColor(kBlue);
   trace[2]->SetLineColor(kPink+10);
   trace[3]->SetLineColor(kCyan);
+
+  tdelta = new TH1F( "tdelta", "time difference distribution", 512, 0, 3*res);
+  tdelta->GetXaxis()->SetTitle("time diff [ns]");
+  tdelta->GetYaxis()->SetTitle("counts");
+
+  h_signal = new TH1F( "h_signal", "signal height distribution", 512, 0, 100);
+  h_signal->GetXaxis()->SetTitle("signal height [mV]");
+  h_signal->GetYaxis()->SetTitle("counts");
+
+
   
-  x2 = new TH2F ( "x2", "Scope persistency plot",1024, -0.5, 1023.5 , 128, -700, 350);
+  x2 = new TH2F ( "x2", "Scope persistency plot",1024, -0.5, 1023.5 , 512, -700, 200);
   x2->SetStats(0);
   x2->SetXTitle("Channel");
   x2->SetYTitle("Signal");
@@ -67,8 +82,9 @@ int process_event (Event * e)
     {
       old_runnumber = e->getRunNumber();
       x2->Reset();
+      tdelta->Reset();
+      h_signal->Reset();
     }
-
 
   
   Packet *p = e->getPacket(packetid);
@@ -97,12 +113,68 @@ int process_event (Event * e)
       for (sample = 0; sample <1024; sample++)
 	{
 	  // x2->Fill ( p->rValue(sample,4),  p->rValue(sample,1));
-	  x2->Fill ( sample,  p->rValue(sample,1));
+	  x2->Fill ( sample,  p->rValue(sample,0) );
 	}
+
+      // the actual analysis part
+
+      double  xsignal = 0;  // "signal" is a keyword
+      double count = 0;  // because I'm lazy
+      int take_this = 1; // we set this to 0 if we see that the signal clips
+      for (sample = 310; sample <1024; sample++)
+	{
+	  float s =  p->rValue(sample,0);
+	  if ( s < -495)
+	    {
+	      take_this = 0;
+	      break;
+	    }
+	  xsignal += s;
+	  count++;
+        }
+      if (take_this)
+	{
+	  xsignal /= count;
+	  xsignal *=-1;
+	  // cout << xsignal << endl;
+	  h_signal->Fill(xsignal);
+	}
+
       delete p;
 
     }
-  //  usleep(2000);
+
+
+  p = e->getPacket(1010, ID4EVT);
+  if (p)
+    {
+      //p->dump();
+
+      // we calculate the difference of the seconds field
+      unsigned long long sec_delta = p->iValue(0) - p->iValue(3);
+
+      // we get the ns value. Since the resolution is 10^9 (1ns),
+      // the upper 32 bit of the 64-bit ns value will always be 0.
+      // but who knows, maybe one day we will get a system with more resolution, so we
+      // do it all the way
+      unsigned long long ns0 = (unsigned int) p->iValue(2);
+      ns0 <<=32;
+      ns0 |= (unsigned int) p->iValue(1);
+
+      // now we add the difference in seconds as nanoseconds
+      ns0 += sec_delta * res;
+      
+      unsigned long long ns1 = (unsigned int) p->iValue(5);
+      ns1 <<=32;
+      ns1 |= (unsigned int) p->iValue(4);
+
+
+      unsigned long long nsdelta = ns0 - ns1;
+      //cout << hex << ns0 << " " << ns1 << dec << "  "  << nsdelta << endl;
+      tdelta->Fill(nsdelta);
+      delete p;
+    }
+
+  
   return 0;
 }
-
